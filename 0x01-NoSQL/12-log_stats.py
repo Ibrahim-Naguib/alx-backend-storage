8f71d729af6f427b28b3ef25e9272445a82a4b3d
= <=>
2024-07-15 12:35:03 -0700

    new commit

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Log stats"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+    all_docs = nginx.count_documents({})
+
+    print("{} logs".format(all_docs))
+    print("Methods:")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    status = nginx.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
